e4cc4b0f6b995278551317552dc5ffc54a69c50a
Ayu <ayu@Ayus-MacBook-Pro.local>
2017-08-10 08:51:45 -0500

    Add meeting imp

--- RM53/MeetingDetailTVC.swift
+++ RM53/MeetingDetailTVC.swift
@@ -8,24 +8,93 @@
 
 import UIKit
 
-class MeetingDetailTVC: UITableViewController {
-
+class MeetingDetailTVC: UITableViewController, UITextViewDelegate {
+    @IBOutlet weak var companyLbl: UILabel!
+    
+    @IBOutlet weak var callPlanLbl: UILabel!
+    @IBOutlet weak var reasonLbl: UILabel!
+    
+    @IBOutlet weak var callReportLbl: UITextView!
+    @IBOutlet weak var detailLbl: UILabel!
+    var isCellHidden = true
+    var isCallReportActive = true
     var meetings = [String: String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.callReportLbl.delegate = self
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 00/255.0, green: 24/255.0, blue: 168/255.0, alpha: 1)
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         navigationItem.title = meetings["company"]
         self.navigationController?.navigationBar.tintColor = UIColor.white
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        let saveButton = UIBarButtonItem(image: UIImage(named: "save2"), style: .plain, target: self, action: #selector(saveItem))
+        
+        let addButton = UIBarButtonItem(image: UIImage(named: "add2"), style: .plain, target: self, action: #selector(addItem))
+        
+         let editButton = UIBarButtonItem(image: UIImage(named: "edit2"), style: .plain, target: self, action: #selector(editItem))
+        
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        //let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveItem))
+        
+        //let editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editItem))
+        if callReportLbl.text != "" {
+            isCellHidden = false
+        }
+        self.navigationItem.rightBarButtonItems = [editButton,addButton,saveButton]
         self.tableView.tableFooterView = UIView()
         self.tableView.isScrollEnabled = false
+        updateRow()
+    }
+    
+    func updateRow(){
+        companyLbl.text = meetings["company"]
+        callPlanLbl.text = meetings["callPlaneName"]
+        reasonLbl.text = meetings["reason"]
+        detailLbl.text = meetings["details"]
+        detailLbl.sizeToFit()
+    }
+    
+    func addItem(){
+        isCellHidden = false
+        isCallReportActive = true
+        callReportLbl.isEditable = true
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+
+    func saveItem(){
+        if callReportLbl.text == "" {
+            isCellHidden = true
+            isCallReportActive = false
+        } else{
+            isCellHidden = false
+            isCallReportActive = false
+        }
+        
+        callReportLbl.isEditable = false
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    func editItem(){
+        //self.setEditing(true, animated: true)
+        if isCellHidden == false && callReportLbl.text != "" {
+            callReportLbl.isEditable = true
+            callReportLbl.becomeFirstResponder()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 4 && isCellHidden {
+            return 0
+        } else {
+            return super.tableView(tableView, heightForRowAt: indexPath)
+        }
     }
 
     // MARK: - Table view data source
@@ -42,9 +111,11 @@ class MeetingDetailTVC: UITableViewController {
 
     
 //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell\(indexPath.row)", for: indexPath)
 //
-//        // Configure the cell...
+//        if indexPath.row == 3 {
+//            
+//        }
 //
 //        return cell
 //    }
